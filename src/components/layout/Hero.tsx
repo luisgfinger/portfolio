4abbfbf7a9c198e9416a3775b878bb17ac392497
gulis007 <luisgfinger@gmail.com>
2026-03-16 18:56:51 -0300

    fix: paragraph size on mobile

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -6,12 +6,12 @@ export function Hero() {
 
   return (
     <section id="home" className="h-screen w-full border-b border-[var(--border-color)] bg-[var(--background)] flex flex-col items-center py-8">
-      <div className="flex flex-col items-center gap-4 max-w-xl">
+      <div className="flex flex-col items-center gap-4">
         <ProfilePicture />
         <h2>Luis Gustavo Grando Finger</h2>
         <p className="text-xl">Engenheiro de Software</p>
         <p className="text-lg">Desenvolvedor Full-Stack</p>
-        <p className="text-base text-center">Desenvolvedor Full Stack focado em criar aplicações escaláveis e soluções em nuvem. 
+        <p className="text-base text-center max-w-sm md:max-w-xl">Desenvolvedor Full Stack focado em criar aplicações escaláveis e soluções em nuvem. 
           Transformo ideias em sistemas robustos utilizando tecnologias modernas e arquitetura cloud.
         </p>
         <div className="flex gap-4">
